255fe51a1d1931935d33ad96c546494fea94492d
treenix <28811732+treenix@users.noreply.github.com>
2018-07-06 15:09:34 +0200

    Basic UI mouse events

--- voxygen/src/ui.rs
+++ voxygen/src/ui.rs
@@ -1,4 +1,5 @@
 use conrod::{
+    self,
     Ui as conrod_ui,
     UiBuilder,
     image::Map,
@@ -19,8 +20,11 @@ use conrod::{
     text::font::Id as fid,
     event::Input,
     backend::gfx::Renderer as ConrodRenderer,
+    input,
 };
 
+use glutin::ElementState;
+use glutin::MouseButton;
 use renderer::Renderer;
 
 use std::collections::HashMap;
@@ -42,7 +46,9 @@ pub struct Ui {
 
 impl Ui {
     pub fn new(renderer: &mut Renderer, size: [f64; 2]) -> Self {
-        let ui = UiBuilder::new(size).build();
+        let mut ui = UiBuilder::new(size).build();
+
+//        ui.handle_event(Input::Motion(input::Motion::MouseCursor {x: 0.0, y: 0.0}));
 
         let image_map = Map::new();
 
@@ -61,11 +67,52 @@ impl Ui {
     }
 
     pub fn render(&mut self, renderer: &mut Renderer, window_size: &[f64; 2]) {
-        self.ui.handle_event(Input::Resize(window_size[0] as u32, window_size[1] as u32));
         self.set_ui();
         self.conrodRenderer.on_resize(renderer.color_view().clone());
         self.conrodRenderer.fill(&mut renderer.encoder_mut(), (window_size[0] as f32 , window_size[1] as f32), 1.0, self.ui.draw(), &self.image_map);
         self.conrodRenderer.draw(&mut renderer.factory_mut().clone(), &mut renderer.encoder_mut(), &self.image_map);
+    }
+
+    fn map_mouse(mouse_button: MouseButton) -> input::MouseButton {
+        match mouse_button {
+            MouseButton::Left => input::MouseButton::Left,
+            MouseButton::Right => input::MouseButton::Right,
+            MouseButton::Middle => input::MouseButton::Middle,
+            MouseButton::Other(0) => input::MouseButton::X1,
+            MouseButton::Other(1) => input::MouseButton::X2,
+            MouseButton::Other(2) => input::MouseButton::Button6,
+            MouseButton::Other(3) => input::MouseButton::Button7,
+            MouseButton::Other(4) => input::MouseButton::Button8,
+            _ => input::MouseButton::Unknown,
+        }
+    }
+
+    pub fn ui_event_window_resize(&mut self, w: u32, h: u32) {
+        self.ui.handle_event(Input::Resize(w, h));
+    }
+
+    pub fn ui_event_mouse_button(&mut self, state: ElementState, button: MouseButton) {
+        let event: Input = match state {
+            ElementState::Pressed => {
+                Input::Press(input::Button::Mouse(Self::map_mouse(button))).into()
+            },
+            ElementState::Released => {
+                Input::Release(input::Button::Mouse(Self::map_mouse(button))).into()
+            }
+        };
+
+        self.ui.handle_event(event);
+    }
+
+    pub fn ui_event_mouse_pos(&mut self, x: f64, y: f64) {
+        let dpi = 1.0;
+        let tx = |x: Scalar| (x / dpi) - self.ui.win_w / 2.0;
+        let ty = |y: Scalar| -((y / dpi) - self.ui.win_h / 2.0);
+
+        let x = tx(x as Scalar);
+        let y = ty(y as Scalar);
+        let motion = input::Motion::MouseCursor { x, y };
+        self.ui.handle_event(Input::Motion(motion).into());
     }
 
     pub fn add_version_number(&mut self) {
@@ -92,9 +139,40 @@ impl Ui {
 
     pub fn set_ui(&mut self) {
         let left_text = self.get_widget_id("left_text");
-
-        let mut ui = self.ui.set_widgets();
+        let range_slider = self.get_widget_id("range_slider");
+        let canvas = self.get_widget_id("canvas");
+        let oval = self.get_widget_id("oval");
         let font = self.fid.unwrap();
+
+        let mut ui = &mut self.ui.set_widgets();
+
+        widget::Canvas::new().color(color::DARK_CHARCOAL).set(canvas, ui);
+
+        const PAD: conrod::Scalar = 20.0;
+        let (ref mut start, ref mut end) = (0.25, 0.75);
+        let min = 0.0;
+        let max = 1.0;
+        for (edge, value) in widget::RangeSlider::new(*start, *end, min, max)
+            .color(color::LIGHT_BLUE)
+            .padded_w_of(canvas, PAD)
+            .h(30.0)
+            .mid_top_with_margin_on(canvas, PAD)
+            .set(range_slider, ui)
+            {
+                match edge {
+                    widget::range_slider::Edge::Start => *start = value,
+                    widget::range_slider::Edge::End => *end = value,
+                }
+            }
+
+        let range_slider_w = ui.w_of(range_slider).unwrap();
+        let w = (*end - *start) * range_slider_w;
+        let h = 200.0;
+        widget::Oval::fill([w, h])
+            .mid_left_with_margin_on(canvas, PAD + *start * range_slider_w)
+            .color(color::LIGHT_BLUE)
+            .down(50.0)
+            .set(oval, ui);
 
         widget::Text::new(&format!("Version {}", env!("CARGO_PKG_VERSION")))
             .font_id(font)
@@ -102,6 +180,7 @@ impl Ui {
             .bottom_left_with_margin(20.0)
             .left_justify()
             .line_spacing(10.0)
-            .set(left_text, &mut ui);
+            .set(left_text, ui);
+
     }
 }
--- voxygen/src/window.rs
+++ voxygen/src/window.rs
@@ -1,7 +1,7 @@
 use gfx_window_glutin;
 use glutin;
 
-use glutin::{EventsLoop, WindowBuilder, ContextBuilder, GlContext, GlRequest, GlWindow, DeviceEvent, WindowEvent, CursorState, MouseCursor};
+use glutin::{Event as glutinEvent, EventsLoop, WindowBuilder, ContextBuilder, GlContext, GlRequest, GlWindow, DeviceEvent, WindowEvent, CursorState, MouseCursor};
 use glutin::Api::OpenGl;
 
 use renderer::{Renderer, ColorFormat, DepthFormat};
@@ -15,6 +15,9 @@ pub enum Event {
     MouseWheel { dx: f64, dy: f64, modifiers: glutin::ModifiersState },
     KeyboardInput { i: glutin::KeyboardInput, device: glutin::DeviceId },
     Resized { w: u32, h: u32 },
+    CursorPosition {x: f64, y: f64},
+    MouseButton { state: glutin::ElementState, button: glutin::MouseButton },
+    Raw { event: glutinEvent },
 }
 
 pub struct RenderWindow {
@@ -56,19 +59,19 @@ impl RenderWindow {
             match event {
                 glutin::Event::DeviceEvent { event, .. } => match event {
                     DeviceEvent::MouseMotion { delta: (dx, dy), .. } => {
-                        if self.cursor_trapped.load(Ordering::Relaxed) {
-                            if let Err(_) = gl_window.set_cursor_state(CursorState::Grab) {
-                                warn!("Could not grap cursor");
-                                self.cursor_trapped.store(false, Ordering::Relaxed)
-                            }
-                            gl_window.set_cursor(MouseCursor::NoneCursor);
-                        } else {
-                            if let Err(_) = gl_window.set_cursor_state(CursorState::Normal) {
-                                warn!("Could not ungrap cursor");
-                                self.cursor_trapped.store(true, Ordering::Relaxed)
-                            }
-                            gl_window.set_cursor(MouseCursor::Default);
-                        }
+//                        if self.cursor_trapped.load(Ordering::Relaxed) {
+//                            if let Err(_) = gl_window.set_cursor_state(CursorState::Grab) {
+//                                warn!("Could not grap cursor");
+//                                self.cursor_trapped.store(false, Ordering::Relaxed)
+//                            }
+//                            gl_window.set_cursor(MouseCursor::NoneCursor);
+//                        } else {
+//                            if let Err(_) = gl_window.set_cursor_state(CursorState::Normal) {
+//                                warn!("Could not ungrap cursor");
+//                                self.cursor_trapped.store(true, Ordering::Relaxed)
+//                            }
+//                            gl_window.set_cursor(MouseCursor::Default);
+//                        }
                         func(Event::CursorMoved { dx, dy });
                     }
                     _ => {},
@@ -114,20 +117,27 @@ impl RenderWindow {
                             i: input,
                         });
                     },
-                    WindowEvent::MouseInput { button, .. } => {
+                    WindowEvent::MouseInput { state, button, .. } => {
                         if button == glutin::MouseButton::Left {
                             self.cursor_trapped.store(true, Ordering::Relaxed);
                             let _ = gl_window.set_cursor_state(CursorState::Grab);
                         }
+
+                        func(Event::MouseButton { state, button });
                     },
                     WindowEvent::CloseRequested => func(Event::CloseRequest),
 
                     WindowEvent::Focused(is_focused) => {
                             self.cursor_trapped.store(is_focused, Ordering::Relaxed);
-                    }
+                    },
+                    WindowEvent::CursorMoved { position, .. } => {
+                        func(Event::CursorPosition {x: position.0, y: position.1 });
+                    },
                     _ => {},
                 },
-                _ => {},
+                _ => {
+                    func(Event::Raw {event});
+                },
             }
         });
     }

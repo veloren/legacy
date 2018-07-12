9b43888f9a446c4434e490dbb98f84c1f3edf31a
Tigralt <tigralt.mail@gmail.com>
2018-07-12 13:40:46 +0200

    Add GIT_TIME env variable (unix timestamp)

--- voxygen/src/build.rs
+++ voxygen/src/build.rs
@@ -1,9 +1,16 @@
 use std::process::Command;
 
 fn main() {
-   // note: add error checking yourself.
-   let output = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
-   let git_hash = String::from_utf8(output.stdout).unwrap();
+    // @todo Error checking
 
-   println!("cargo:rustc-env=GIT_HASH={}", git_hash); // Store as GIT_HASH env variable
+    // Git hash
+    let output_hash = Command::new("git").args(&["rev-parse", "HEAD"]).output().unwrap();
+    let git_hash = String::from_utf8(output_hash.stdout).unwrap();
+
+    // Git time
+    let output_time = Command::new("git").args(&["log", "-1", "--pretty=format:%ct"]).output().unwrap();
+    let git_time = String::from_utf8(output_time.stdout).unwrap();
+
+    println!("cargo:rustc-env=GIT_HASH={}", git_hash); // Store as GIT_HASH env variable
+    println!("cargo:rustc-env=GIT_TIME={}", git_time); // Store as GIT_TIME env variable
 }
